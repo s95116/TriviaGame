7683bf879a880f6a927670f25a5915d47aa52219
Seth Keel <seth.keel@gmail.com>
2016-06-04 19:54:41 -0400

    fourth commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
 
-	var counter = 4;
+	var counter = 10;
+	var questions = [];
+
+	function questionsObject(q, c1, c2, c3, c4, a) {
+		this.question = q;
+		this.choices = [c1, c2, c3, c4];
+		this.answer = a;
+	}
+
+	questions.push(new questionsObject('Who voices the computer in Star Trek: The Next Generation?',
+	'Sandra Bullock', 'Majel Barrett', 'Tina Fey', 'David Livingston', 1 ));
+
 
 	$('#startButton').on('click', function() {
   		$('#main-panel').empty();
@@ -9,32 +20,39 @@ $(document).ready(function() {
 				
 				//Code the refreshes the number every second
 				//Reduce the number of seconds left by 1
-				counter--;
-				$('#main-panel').html('Time Remaining: ' + counter);
-				// $('#startButton').html('' + counter);
 				
+				$('#main-panel').text('Time Remaining: ' + counter);
+					counter--;
+
+
+
+
+				$('#main-panel').append('<p></p>' + questions[0].question);
+	  			$('#main-panel').append('<p id="c1" class="choices"> Sandra Bullock </p>');
+	  			$('#main-panel').append('<p id="c2" class="choices"> Majel Barrett</p>');
+	  			$('#main-panel').append('<p id="c3" class="choices"> Tina Fey </p>');
+	  			$('#main-panel').append('<p id="c4" class="choices"> David Livingston </p>');
+				
+	  			// $('.choices').on('click', function() {
+	  			$('#c1').on('click', function() {
+	  				alert('Choices has been clicked.');
+
+	  			});
+
+
+
 				//Shows the counter
 
-				if (counter === 0){
+				if (counter === -1){
 					alert("Time's Up!");
 					clearInterval(intervalId);
 
 					//Logic to reveal correct answer goes here
 				}
 				console.log(counter);
+
+				
 				}, 1000);
-
-	  			
-	  		$('#main-panel').html( "<p>Test</p>" );
-	  		$('#main-panel').html( "<p>Test</p>" );
-	  		$('#main-panel').html( "<p>Test</p>" );
-	  		$('#main-panel').html( "<p>Test</p>" );
-
-
-
-	
-
-
 
 	}); //End of #startButton 'on.click' function
 
